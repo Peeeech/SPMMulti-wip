2f68632e3905ebb57a6c3ed5505b50b5ea104cd0
Peeeech <159792426+Peeeech@users.noreply.github.com>
2026-02-01 09:49:12 -0600

    added `ridx` command references

--- include/commands.h
+++ include/commands.h
@@ -14,6 +14,8 @@
 namespace mod {
 
 u32 handleItemBinary(const u8* payload, size_t payloadLen, u8* response, size_t responseSize);
+u32 handleIdxBinary(const u8* payload, size_t payloadLen, u8* response, size_t responseSize);
+u32 readMemoryIdx(u8* response, size_t responseSize);
 
 #define COMMAND(id, name, description, argc, code) \
     Command name(id, #name, description, argc, [](eastl::vector<const char*> &args, u8* response, size_t responseSize) -> u32 code);
@@ -23,6 +25,8 @@ extern Command write;
 
 extern Command msgbox;*/
 extern Command item;
+extern Command idx;
+extern Command ridx;
 /*EVT_DECLARE(msgbox_cmd)
 EVT_DECLARE(fwd_msgbox_cmd)
 EVT_DECLARE_USER_FUNC(evt_post_msgbox, 1)*/
