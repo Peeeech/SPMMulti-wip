ae20983c25599f92d099fe8a4df920af786d06d2
Peeeech <159792426+Peeeech@users.noreply.github.com>
2026-02-09 13:39:32 -0600

    pray for me

--- include/commands.h
+++ include/commands.h
@@ -17,6 +17,7 @@ namespace mod {
 u32 handleItemBinary(const u8* payload, size_t payloadLen, u8* response, size_t responseSize);
 u32 handleIdxBinary(const u8* payload, size_t payloadLen, u8* response, size_t responseSize);
 u32 readMemoryIdx(u8* response, size_t responseSize);
+u32 readMemoryBusy(u8* response, size_t responseSize);
 
 #define COMMAND(id, name, description, argc, code) \
     Command name(id, #name, description, argc, [](eastl::vector<const char*> &args, u8* response, size_t responseSize) -> u32 code);
@@ -28,6 +29,7 @@ extern Command msgbox;*/
 extern Command item;
 extern Command idx;
 extern Command ridx;
+extern Command rbusy;
 /*EVT_DECLARE(msgbox_cmd)
 EVT_DECLARE(fwd_msgbox_cmd)
 EVT_DECLARE_USER_FUNC(evt_post_msgbox, 1)*/
